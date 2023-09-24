6bdf2bf479479d8ee9a5d2059099644837c28fc0
Akshay <akshaywairagade2@gmail.com>
2023-09-24 19:53:01 +0530

    Connected to DataBase

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
-const express=require('express')
+const express=require('express');
 const app=express();
+const mongoose=require('mongoose');
+const env=require('dotenv');
 
+env.config();
 
-app.get('/',(req,res)=>{
-    res.send("Hello World");
-})
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.jy0uzsn.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser:true,
+        useUnifiedTopology: true,
+        // useCreateIndex:true
+    }
+).then(()=>{
+    console.log(`Database Connected`);
+});
 
-app.listen(5000,()=>{
+app.listen(process.env.PORT_NUMBER,()=>{
     console.log("Connected");
 })
